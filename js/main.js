e5c77e1a5be05974affc2af237a5b64bae4d3617
RookieYang <yy335281965@gmail.com>
2018-11-13 17:29:26 +0800

    代码

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(p1).on('click',function(){
+  $(images).css({
+    transform: 'translateX(0)'
+  })
+})
+$(p2).on('click',function(){
+  $(images).css({
+    transform: 'translateX(-400px)'
+  })
+})
+$(p3).on('click',function(){
+  $(images).css({
+    transform: 'translateX(-800px)'
+  })
+})
+$(p4).on('click',function(){
+  $(images).css({
+    transform: 'translateX(-1200px)'
+  })
+})
